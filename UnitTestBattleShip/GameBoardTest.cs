fb9133716c5e93f684cdf9248893fcde49ccdae7
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-21 10:27:38 -0700

    Added a test to test the setter of the class

--- UnitTestBattleShip/GameBoardTest.cs
+++ UnitTestBattleShip/GameBoardTest.cs
@@ -1,13 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BattleShip.Models;
 
 namespace UnitTestBattleShip
 {
     [TestClass]
     public class GameBoardTest
     {
+       
         [TestMethod]
-        public void TestMethod1()
+        public void TestIsInRange()
         {
+            GameBoard gameBoard = new GameBoard();
+            bool results = gameBoard.IsShipInRange("b3b4b5");
+           
+
         }
+
+        [TestMethod]
+        public void TestClassSetters()
+        {
+            GameBoard gameBoard = new GameBoard();
+            //bool results = gameBoard.IsShipInRange("b3b4b5");
+            gameBoard.Height = 6;
+
+            Assert.AreEqual(gameBoard.Height, 6);
+
+        }
+
+
+
     }
 }
