c4ef04cf3757a333a9c0d4849c540ab9c5ac2732
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-21 00:11:52 -0700

    created the test file for GameBoard Class

--- UnitTestBattleShip/GameBoardTest.cs
+++ UnitTestBattleShip/GameBoardTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestBattleShip
 {
     [TestClass]
-    public class UnitTest1
+    public class GameBoardTest
     {
         [TestMethod]
         public void TestMethod1()
