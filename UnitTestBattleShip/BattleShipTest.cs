8d9c49bd2396769c42c78a675efeb67bb02658fd
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-22 01:02:05 -0700

    Formatting and cleaning the tests

--- UnitTestBattleShip/BattleShipTest.cs
+++ UnitTestBattleShip/BattleShipTest.cs
@@ -7,15 +7,31 @@ namespace UnitTestBattleShip
     [TestClass]
     public class BattleShipTest
     {
-       
-        
+ 
+        [TestMethod]
+        public void GetShipSlots_ValueForShipString_ReturnTheSameValue()
+        {
+            Ship battleShip = new Ship();
+            battleShip.ShipString = "a1a2a3";
+            Assert.AreEqual(String.Join("", battleShip.GetShipSlots()), "a1a2a3" );
+
+        }
 
         [TestMethod]
-        public void TestShipSetters()
+        public void ShipSetters_ValueForLength_ReturnTheSameValue()
         {
-            Ship battleShip = new Ship("a1a2a3");
+            Ship battleShip = new Ship();
             battleShip.Length = 6;
-            Assert.AreEqual(String.Join("", battleShip.ShipSlots), "a1a2a3" );
+            Assert.AreEqual(battleShip.Length, 6);
+
+        }
+
+        [TestMethod]
+        public void ShipSetters_ValueForShipString_ReturnTheSameValue()
+        {
+            Ship battleShip = new Ship();
+            battleShip.ShipString = "a1b2c3";
+            Assert.AreEqual(battleShip.ShipString, "a1b2c3");
 
         }
 
--- UnitTestBattleShip/GameBoardTest.cs
+++ UnitTestBattleShip/GameBoardTest.cs
@@ -9,14 +9,44 @@ namespace UnitTestBattleShip
     {
        
         [TestMethod]
-        public void TestIsInRange()
+        public void IsShipInRange_ValidShipString_ReturnTrue()
         {
             GameBoard gameBoard = new GameBoard();
-            bool results = gameBoard.IsShipInRange("b3b4b5");
+            bool results = gameBoard.IsShipInRange("a1b2H3");
             Assert.IsTrue(results);
-           
-
         }
+        [TestMethod]
+        public void IsShipInRange_ValidShotFired_ReturnTrue()
+        {
+            GameBoard gameBoard = new GameBoard();
+            bool results = gameBoard.IsShipInRange("b8");
+            Assert.IsTrue(results);
+        }
+
+        [TestMethod]
+        public void IsShipInRange_InvalidShotFiredWidthOutofRange_ReturnFalse()
+        {
+            GameBoard gameBoard = new GameBoard();
+            bool results = gameBoard.IsShipInRange("i2");
+            Assert.IsFalse(results);
+        }
+
+        [TestMethod]
+        public void IsShipInRange_InvalidShotFiredHeightOutofRange_ReturnFalse()
+        {
+            GameBoard gameBoard = new GameBoard();
+            bool results = gameBoard.IsShipInRange("b9");
+            Assert.IsFalse(results);
+        }
+
+        [TestMethod]
+        public void IsShipInRange_InvalidShotFiredHeightOfZero_ReturnFalse()
+        {
+            GameBoard gameBoard = new GameBoard();
+            bool results = gameBoard.IsShipInRange("b0");
+            Assert.IsFalse(results);
+        }
+
 
         [TestMethod]
         public void TestBoardSetters()
