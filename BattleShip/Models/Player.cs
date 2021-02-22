58c8496d1f0ab1f15ebf8c4541c202d03f5aad99
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-22 00:59:32 -0700

    Added Player Class

---
+++ BattleShip/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShip.Models
+{
+    //A class to create player object that has a battleShipBoard object and a battleShip object
+    public class Player : IPlayer
+    {
+        public IGameBoard playerBoard { get; set; } = new GameBoard();
+        public IShip playerShip { get; set; } = new Ship();
+    }
+}
