50f2967d2f32069053d55d5f23a494263f7881dd
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-22 01:08:34 -0700

    added the Autofac Container Configuration class

---
+++ BattleShip/ContainerConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BattleShip
+{
+    //Class to congofure Autofac Container for Dependency Injection
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            //Init the container builder
+            var builder = new ContainerBuilder();
+            //Register all the classes as their interfaces
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(BattleShip)))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+            //Build the container
+            return builder.Build();
+        }
+    }
+}
